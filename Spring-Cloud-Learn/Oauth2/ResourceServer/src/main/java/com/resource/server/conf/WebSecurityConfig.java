46e7521b8b174531541698c7fd80c42c2a11c61f
Edge <Edge@Edge-PC>
2020-08-23 10:25:41 +0800

    安全访问控制

---
+++ Spring-Cloud-Learn/Oauth2/ResourceServer/src/main/java/com/resource/server/conf/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.resource.server.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/*安全访问控制*/
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable().authorizeRequests().antMatchers("").hasAnyAuthority("");
+		
+		super.configure(http);
+	}
+	
+}
