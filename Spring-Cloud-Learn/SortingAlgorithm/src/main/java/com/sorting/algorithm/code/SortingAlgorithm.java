4815ebe5162b4460ca586460ddcf1341294340f3
Edge <Edge@Edge-PC>
2020-09-04 09:20:46 +0800

    冒泡排序

---
+++ Spring-Cloud-Learn/SortingAlgorithm/src/main/java/com/sorting/algorithm/code/SortingAlgorithm.java
@@ -0,0 +1,26 @@
+package com.sorting.algorithm.code;
+
+public class SortingAlgorithm {
+
+	/**
+	 * 冒泡排序
+	 * 
+	 * @param array
+	 * @return
+	 */
+	public static int[] bubbleSort(int[] array) {
+		if (array.length == 0) {
+			return array;
+		}
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length - 1 - i; j++) {
+				if (array[j + 1] < array[j]) {
+					int temp = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+		return array;
+	}
+}
