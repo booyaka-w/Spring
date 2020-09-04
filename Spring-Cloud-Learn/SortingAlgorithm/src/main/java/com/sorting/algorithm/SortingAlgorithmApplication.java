e16206f4ca9d1dfd72a39e7b306c53b9b253b6e2
Edge <Edge@Edge-PC>
2020-09-04 08:16:45 +0800

    package

--- Spring-Cloud-Learn/SortingAlgorithm/src/main/java/com/sorting/algorithm/SortingAlgorithmApplication.java
+++ Spring-Cloud-Learn/SortingAlgorithm/src/main/java/com/sorting/algorithm/SortingAlgorithmApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.sorting.algorithm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Spring-Cloud-Learn/SortingAlgorithm/src/test/java/com/sorting/algorithm/SortingAlgorithmApplicationTests.java
+++ Spring-Cloud-Learn/SortingAlgorithm/src/test/java/com/sorting/algorithm/SortingAlgorithmApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.sorting.algorithm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
