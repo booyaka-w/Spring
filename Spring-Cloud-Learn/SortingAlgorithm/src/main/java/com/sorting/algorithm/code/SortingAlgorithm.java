0c929e66dc05119e0e70565a4aac3f7b4f8b8fd7
Edge <Edge@Edge-PC>
2020-09-05 19:03:13 +0800

    选择排序

--- Spring-Cloud-Learn/SortingAlgorithm/src/main/java/com/sorting/algorithm/code/SortingAlgorithm.java
+++ Spring-Cloud-Learn/SortingAlgorithm/src/main/java/com/sorting/algorithm/code/SortingAlgorithm.java
@@ -23,4 +23,28 @@ public class SortingAlgorithm {
 		}
 		return array;
 	}
+
+	/**
+	 * 选择排序
+	 * 
+	 * @param array
+	 * @return
+	 */
+	public static int[] selectionSort(int[] array) {
+		if (array.length == 0)
+			return array;
+		for (int i = 0; i < array.length; i++) {
+			int minIndex = i;
+			for (int j = i; j < array.length; j++) {
+				// 找到最小的数
+				if (array[j] < array[minIndex])
+					// 将最小数的索引保存
+					minIndex = j;
+			}
+			int temp = array[minIndex];
+			array[minIndex] = array[i];
+			array[i] = temp;
+		}
+		return array;
+	}
 }
