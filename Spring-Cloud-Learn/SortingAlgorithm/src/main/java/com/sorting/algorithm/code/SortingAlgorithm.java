6610aa243175afc239ad35ce3107e5b73beaf791
Edge <Edge@Edge-PC>
2020-09-08 08:24:02 +0800

    希尔排序

--- Spring-Cloud-Learn/SortingAlgorithm/src/main/java/com/sorting/algorithm/code/SortingAlgorithm.java
+++ Spring-Cloud-Learn/SortingAlgorithm/src/main/java/com/sorting/algorithm/code/SortingAlgorithm.java
@@ -72,4 +72,28 @@ public class SortingAlgorithm {
 		}
 		return array;
 	}
+
+	/**
+	 * 4，希尔排序
+	 *
+	 * @param array
+	 * @return
+	 */
+	public static int[] ShellSort(int[] array) {
+		int len = array.length;
+		int temp, gap = len / 2;
+		while (gap > 0) {
+			for (int i = gap; i < len; i++) {
+				temp = array[i];
+				int preIndex = i - gap;
+				while (preIndex >= 0 && array[preIndex] > temp) {
+					array[preIndex + gap] = array[preIndex];
+					preIndex -= gap;
+				}
+				array[preIndex + gap] = temp;
+			}
+			gap /= 2;
+		}
+		return array;
+	}
 }
