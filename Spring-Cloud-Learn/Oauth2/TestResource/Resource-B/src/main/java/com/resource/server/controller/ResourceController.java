bab6e09168dd100131d9099a2640e8f15d3d94f3
Edge <Edge@Edge-PC>
2020-09-21 15:56:30 +0800

    redisson

--- Spring-Cloud-Learn/Oauth2/TestResource/Resource-B/src/main/java/com/resource/server/controller/ResourceController.java
+++ Spring-Cloud-Learn/Oauth2/TestResource/Resource-B/src/main/java/com/resource/server/controller/ResourceController.java
@@ -4,17 +4,21 @@ import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.resource.server.commons.RedisLockUtil;
 import com.resource.server.model.TestTable;
 import com.resource.server.service.TestTableService;
 
 @RestController
 public class ResourceController {
+
+	@Autowired
+	RedissonClient redisson;
 
 	@Autowired
 	TestTableService testTableService;
@@ -34,20 +38,24 @@ public class ResourceController {
 	@RequestMapping("/update")
 	public String updateTable(HttpServletRequest request) throws Exception {
 		int port = request.getServerPort();
+		System.err.println(redisson);
+		RLock rlock = redisson.getLock("lock");
+		boolean state = rlock.tryLock(300, 300, TimeUnit.SECONDS);
 		TestTable table = testTableService.query(1);
 		try {
-			RedisLockUtil.tryLock(String.valueOf(table.getPk()), TimeUnit.SECONDS, 30, 30);
-			if (table.getNumber() > 0) {
-				table.setNumber(table.getNumber() - 1);
-				table.setPort(port + "");
-				int res = testTableService.update(table);
-				if (res == 1) {
-					testTableService.insert(table);
+			if (state) {
+				if (table.getNumber() > 0) {
+					table.setNumber(table.getNumber() - 1);
+					table.setPort(port + "");
+					int res = testTableService.update(table);
+					if (res == 1) {
+						testTableService.insert(table);
+					}
 				}
 			}
 		} catch (Exception e) {
 		} finally {
-			RedisLockUtil.unlock(String.valueOf(table.getPk()));
+			rlock.unlock();
 		}
 		return "port = " + port;
 	}
