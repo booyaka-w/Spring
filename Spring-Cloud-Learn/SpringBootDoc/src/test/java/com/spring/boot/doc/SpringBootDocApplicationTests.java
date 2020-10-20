4b6817911b7e0d8603096363ada6b6edfcbbe4d4
Edge <Edge@Edge-PC>
2020-10-20 15:01:57 +0800

    Doc

---
+++ Spring-Cloud-Learn/SpringBootDoc/src/test/java/com/spring/boot/doc/SpringBootDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
